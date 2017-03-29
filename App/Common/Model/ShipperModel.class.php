7eea34544eeb79bb9b9d24119196b728baa65d72
zhangxin <zhangxin@51kupai.com>
2017-03-29 16:18:11 +0800

    完成物流功能

--- App/Common/Model/ShipperModel.class.php
+++ App/Common/Model/ShipperModel.class.php
@@ -1,26 +1,38 @@
 <?php
-namespace Common\Model;
-use Think\Model;
-/*****
- *		文章模型
- *		editor	zhangxin
- *		date		2015-5-6 13:34:57
- *****/
-class ShipperModel extends Model {
-	
 
-	const STATUS_ENABLE="1";
-	const STATUS_DISABLE="2";
-	protected $_validate = array(
-     array('name','require','物流名称必须填写'),  // 都有时间都验证
-   );
-	public static $STATUS_MAP=array(
-		self::STATUS_ENABLE=>'启用',
-		self::STATUS_DISABLE=>'禁用',
-		);
+    namespace Common\Model;
+
+    use Think\Model;
+
+    /*****
+     *        文章模型
+     *        editor    zhangxin
+     *        date        2015-5-6 13:34:57
+     *****/
+    class ShipperModel extends Model
+    {
 
 
+        const STATUS_ENABLE  = "1";
+        const STATUS_DISABLE = "2";
+        protected     $_validate  = array(
+            array('name', 'require', '物流名称必须填写'),  // 都有时间都验证
+        );
+        public static $STATUS_MAP = array(
+            self::STATUS_ENABLE => '启用',
+            self::STATUS_DISABLE => '禁用',
+        );
+
+        /**
+         * 获取物流数据
+         * @param $id
+         * @return mixed
+         */
+        public function getShipperInfoById($id){
+            return $this->where(['id'=>$id])->find();
+        }
 
 
-}
-?>
+    }
+
+    ?>
--- App/Manager/Controller/LogisticsController.class.php
+++ App/Manager/Controller/LogisticsController.class.php
@@ -2,6 +2,8 @@
 
     namespace Manager\Controller;
 
+    use Common\Model\ShipperModel;
+
     class  LogisticsController extends AdminBaseController
     {
 
@@ -10,9 +12,72 @@
         public function __construct()
         {
             parent::__construct();
+            $this->model = new ShipperModel();
 
         }
-        public function index(){
+
+        public function index()
+        {
+            $data = $this->page_com($this->model, ['id' => 'desc']);
+            foreach ($data['list'] as $k => $v) {
+                $data['list'][$k]['statusName'] = ShipperModel::$STATUS_MAP[$v['status']];
+            }
+            $this->assign('data', $data);
             $this->display();
+        }
+
+        public function add()
+        {
+            if (IS_POST) {
+                $data = I('post.');
+                $data['create_time'] = date('Y-m-d H:i:s');
+                if (!$this->model->create($data)) {
+                    $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => $this->model->getError()));
+                }
+                if (!$this->model->add()) {
+                    $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => "添加失败"));
+                }
+                $this->ajaxReturn(array('error' => self::SUCCESS_NUMBER, 'message' => "添加成功"));
+            } else {
+
+                $this->display();
+            }
+        }
+
+        public function edit()
+        {
+            $id = I('id');
+            if (IS_POST) {
+                if ($id <= 0) {
+                    $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => "不合法请求"));
+                }
+                $data['name'] = I('post.name');
+                if (!$this->model->create($data)) {
+                    $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => $this->model->getError()));
+                }
+                if (!$this->model->where(['id' => $id])->save()) {
+                    $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => "修改失败"));
+                }
+                $this->ajaxReturn(array('error' => self::SUCCESS_NUMBER, 'message' => "修改成功"));
+            } else {
+                if ($id <= 0) {
+                    $this->error("不合法请求", U('Logistics/index'));
+                }
+                $this->assign('info', $this->model->getShipperInfoById($id));
+                $this->display();
+            }
+        }
+
+        public function del()
+        {
+            if (($id = I('id', 0, 'intval')) <= 0) {
+                $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => "数据格式有误"));
+            }
+
+            $status = intval(I('status', 0, 'intval')) == ShipperModel::STATUS_ENABLE ? ShipperModel::STATUS_DISABLE : ShipperModel::STATUS_ENABLE;
+            if (!$this->model->where(array('id' => $id))->save(array('status' => $status))) {
+                $this->ajaxReturn(array('error' => self::ERROR_NUMBER, 'message' => '操作失败'));
+            }
+            $this->ajaxReturn(array('error' => self::SUCCESS_NUMBER, 'message' => '操作成功'));
         }
     }
