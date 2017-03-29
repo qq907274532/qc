792207ee0e7d9cb65c9a8d569073cb6661894b64
zhangxin <zhangxin@51kupai.com>
2017-03-29 15:12:02 +0800

    数据库配置加入代码

--- App/Manager/Controller/LogisticsController.class.php
+++ App/Manager/Controller/LogisticsController.class.php
@@ -6,13 +6,10 @@
     {
 
         private $model;
-        private $articleCateModel;
 
         public function __construct()
         {
             parent::__construct();
-            $this->model = new ArticleModel();
-            $this->articleCateModel = new ArticleCateModel();
 
         }
         public function index(){
---
+++ Data/conf/db.php
@@ -0,0 +1,10 @@
+<?php
+  return [
+      'DB_TYPE' => 'mysql',
+      'DB_HOST' => 'localhost',
+      'DB_NAME' => 'qianchao',
+      'DB_USER' => 'root',
+      'DB_PWD' => '',
+      'DB_PORT' => '3306',
+      'DB_PREFIX' => 'ms_',
+  ];
